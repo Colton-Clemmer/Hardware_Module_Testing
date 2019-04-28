bba2e989d614771c7dc3cd5724989ebc64f767a7
orbweaver- <cole.clemmer@gmail.com>
2019-04-28 14:02:03 -0700

    better delays

--- shift_register.ino
+++ shift_register.ino
@@ -1,43 +1,41 @@
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
-int serial = 0;
-int clk = 1;
-int reset = 2;
+int serial = 13;
+int clk = 12;
+int reset = 11;
+int disable = 10;
 
-int btnUp = 7;
-int btnReset = 6;
-
-int btnUpLed = 9;
-
-double delayTime = .001;
+int delayTime = 400;
 int index = 1;
 
 void resetRegister() {
   digitalWrite(reset, HIGH);
-  delay(delayTime);
+  delayMicroseconds(delayTime);
   digitalWrite(reset, LOW);
-  delay(delayTime);
+  delayMicroseconds(delayTime);
   digitalWrite(reset, HIGH);
-  delay(delayTime);
+  delayMicroseconds(delayTime);
 }
 
 void writeBit(boolean b) {
   digitalWrite(clk, LOW);
-  delay(delayTime);
+  delayMicroseconds(delayTime);
   digitalWrite(serial, b ? HIGH : LOW);
-  delay(delayTime);
+  delayMicroseconds(delayTime);
   digitalWrite(clk, HIGH);
-  delay(delayTime);
+  delayMicroseconds(delayTime);
   digitalWrite(clk, LOW);
-  delay(delayTime);
+  delayMicroseconds(delayTime);
 }
 
 
 void writeByte(int b) {
   resetRegister();
-  for (int i = 0;i < 16;i++) {
+  digitalWrite(disable, HIGH);
+  for (int i = 0;i < 24;i++) {
     writeBit((b >> i) & 1);
   }
+  digitalWrite(disable, LOW);
 }
 
 
@@ -47,37 +45,22 @@ void setup() {
   pinMode(serial, OUTPUT);
   pinMode(clk, OUTPUT);
   pinMode(reset, OUTPUT);
+  pinMode(disable, OUTPUT);
   
-  pinMode(btnUpLed, OUTPUT);
-  
-  pinMode(btnUp, INPUT);
-  pinMode(btnReset, INPUT);
- 
+  digitalWrite(disable, LOW);
   digitalWrite(clk, LOW);
   resetRegister();
-  writeByte(index);
+  writeByte(1);
 }
 
 int upState = 0;
 
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(btnUpLed, digitalRead(btnUp));
-  int oldUpState = upState;
-  upState = digitalRead(btnUp);
-  if (oldUpState != upState && upState == LOW) {
-    index++;
-    writeByte(index);
-  }
-  if (digitalRead(btnReset) == HIGH) {
-    index = 1;
-    resetRegister();
-    writeByte(1);
-  }
   index++;
   writeByte(index);
-  delay(1);
-  if (index > 62000) {
+  delay(20);
+  if (index > 255) {
     resetRegister();
     index = 1;
   }
